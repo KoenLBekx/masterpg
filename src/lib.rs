6dda02a38084c099444e521ae6d4234b7c1697f9
Koen Bekx <klbekx@gmail.com>
2023-07-21 17:21:18 +0300

    Added unit tests in mod tests::enveloppe_same_name on an experimental parser that finds nested named marks.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::ffi::OsStr;
 use nom::{
     branch::alt,
-    bytes::complete::{is_a, is_not, tag},
+    bytes::complete::{is_a, is_not, tag, take},
     character::complete::char,
     combinator::{map, opt, peek, recognize},
     Err,
@@ -106,6 +106,13 @@ fn master_page_closing_tag(input: &str) -> IResult<&str, String> {
         |found| String::from(found)
     )(input)
 }
+
+/*
+fn master_page_opening_closing_tag(input: &str) -> IResult<&str, (Vec<String>,  String)> {
+    pair(
+    )(input)
+}
+*/
 
 pub fn compose<Tioh>(file_path:&OsStr, io_handler:Tioh) -> Result<(), Box<dyn std::error::Error>>
 where Tioh: TextIOHandler {
@@ -265,5 +272,189 @@ mod tests {
         */
 
         assert!(master_page_closing_tag("</+ \t ac/tual >").is_err());
+    }
+
+    mod enveloppe_same_name {
+        use std::cell::RefCell;
+        use std::rc::Rc;
+        use nom::{
+            branch::alt,
+            bytes::complete::{is_not, tag, take},
+            character::complete::alphanumeric1,
+            combinator::{map, verify},
+            error::Error,
+            IResult,
+            multi::many0,
+            sequence::{preceded, terminated, tuple},
+        };
+
+        macro_rules! make_open_mark {
+            ($name_stack:expr, $is_first:expr) => {
+                verify(
+                    preceded(tag::<&str, &str, Error<&str>>("<"), take(1u8)),
+                    |name: &str| {
+                        match name {
+                            "<" | ">" => false,
+                            nm =>  {
+                                if $is_first && ($name_stack.borrow().len() > 0) {
+                                    return false;
+                                }
+
+                                if (!$is_first) && ($name_stack.borrow().len() < 1) {
+                                    return false;
+                                }
+
+                                $name_stack.borrow_mut().push(name.to_string());
+
+                                true
+                            }
+                        }
+                    }
+                )
+            }
+        }
+
+        macro_rules! make_close_mark {
+            ($name_stack:expr, $is_last:expr) => {
+                verify(
+                    preceded(tag::<&str, &str, Error<&str>>(">"), take(1u8)),
+                    |name: &str|
+                    {
+                        match name {
+                            "<" | ">" => false,
+                            nm =>  {
+                                let mut stack_mut = $name_stack.borrow_mut();
+
+                                // println!("Before verify on close : name={}, name_stack={:?}", nm, stack_mut);
+
+                                if $is_last && (stack_mut.len() != 1) {
+                                    return false;
+                                }
+
+                                if (!$is_last) && (stack_mut.len() < 2) {
+                                    return false;
+                                }
+
+                                match stack_mut.last() {
+                                    None => false,
+                                    Some(ref last_name) => {
+                                        if nm == last_name.as_str() {
+                                            stack_mut.pop();
+                                            true
+                                        } else {
+                                            false
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                )
+            }
+        }
+
+        #[test]
+        fn nom_open_close_same_name() {
+            /* Given opening and closing marks of the form
+             * <alpha and >alpha,
+             * checks if
+             * <a___>a
+             * is recognized as one entity, but not
+             * <a___>b
+             * while
+             * <a___<b___>b___>a
+             * is recognized as such again.
+             */
+
+            let name_stack: RefCell<Vec<String>> = RefCell::new(Vec::new());
+            
+            let mut open_mark_first = make_open_mark!(name_stack, true);
+            let mut open_mark_nested = make_open_mark!(name_stack, false);
+
+            assert_eq!(Ok(("12", "a")), open_mark_first("<a12"));
+
+            let mut close_mark_last = make_close_mark!(name_stack, true);
+            let mut close_mark_nested = make_close_mark!(name_stack, false);
+
+            assert_eq!(Ok(("zz", "a")), close_mark_last(">azz"));
+
+            let mut enveloppe = map(
+                tuple((
+                    open_mark_first,
+                    many0(alt((
+                        is_not("<>"),
+                        open_mark_nested,
+                        close_mark_nested,
+                    ))),
+                    close_mark_last
+                )),
+                |tp|
+                {
+                    let mut result = Vec::<&str>::new();
+                    result.push(tp.0);
+                    result.extend(tp.1.iter());
+                    result.push(tp.2);
+
+                    result
+                }
+            );
+
+            name_stack.borrow_mut().clear();
+            assert_eq!(
+                Ok(("", vec!["x", "_-_-_", "x"])),
+                enveloppe("<x_-_-_>x")
+            );
+
+            name_stack.borrow_mut().clear();
+            assert_eq!(
+                Ok(("", vec!["x", "_", "x", "_", "x", "x"])),
+                enveloppe("<x_<x_>x>x")
+            );
+
+            name_stack.borrow_mut().clear();
+            assert_eq!(
+                Ok(("", vec!["x", "x"])),
+                enveloppe("<x>x")
+            );
+
+            name_stack.borrow_mut().clear();
+            assert_eq!(
+                Ok(("", vec!["x", "_", "y", "-", "y", "_", "x"])),
+                enveloppe("<x_<y->y_>x")
+            );
+
+            name_stack.borrow_mut().clear();
+            assert_eq!(
+                Ok(("---<a--->a", vec!["x", "_", "y", "-", "y", "_", "x"])),
+                enveloppe("<x_<y->y_>x---<a--->a")
+            );
+
+            name_stack.borrow_mut().clear();
+            assert!(enveloppe("___<").is_err());
+
+            name_stack.borrow_mut().clear();
+            assert!(enveloppe("<<___><").is_err());
+
+            name_stack.borrow_mut().clear();
+            assert!(enveloppe("<x___").is_err());
+
+            name_stack.borrow_mut().clear();
+            assert!(enveloppe(">x").is_err());
+
+            name_stack.borrow_mut().clear();
+            assert!(enveloppe("<x___>y").is_err());
+
+            name_stack.borrow_mut().clear();
+            assert!(enveloppe("<x__<x__>x").is_err());
+
+            name_stack.borrow_mut().clear();
+            assert!(enveloppe("<x__<z__>x").is_err());
+
+            name_stack.borrow_mut().clear();
+            assert!(enveloppe("<x__>z__>x").is_err());
+
+            name_stack.borrow_mut().clear();
+            assert!(enveloppe("<x__<z__>x__>z").is_err());
+        }
     }
 }
