a093c32259d3785396b12d12ef1bc73e2e5e5de0
Koen Bekx <klbekx@gmail.com>
2023-10-23 23:42:57 +0300

    Added documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,189 @@
-// TODO: Add documentation in code.
 // TODO: Change version to 1.0.0 after doing the same for tree_by_path and string_io_and_mock.
-// TODO?: Add a man page.
-// TODO?: Add more operators to Calculation. (See std::f64)
-//              round, floor, ceiling, trunc, ?(if 1 then 2 else 3)
+
+//{ Documentation
+//! # `masterpg` utility
+//!
+//! `fn main` in src/main.rs<br >
+//! calls<br />
+//! `pub fn compose` in src/lib.rs.
+//!
+//! `pub fn compose` composes files - usually static HTML files - as an alternative to a missing
+//! `<include />` tag in HTML.
+//! 
+//! It processes six kinds of tags in input files :
+//! 
+//! > `<+output file_path/>` :
+//! >> indicates to which file the result of the composition operation has to be written.
+//! 
+//! > `<+master file_path/>` :
+//! >> indicates that a file having the given path is to be used as master page. If the file path doesn't contain a full path, it is considered to be relative to  the current active directory.<br />
+//! An input file can contain more than one `<+master ...>` tag.
+//!
+//! > `<+placeholder tag_name/>` :
+//! >> indicates that this placeholder has to be replaced by the content of an `<+actual>` tag having the same tag name.
+//!
+//! > `<+actual tag_name>`...`</+actual>` :
+//! >> the actual content to replace a placeholder having the same tag name.
+//! 
+//! > `<+comment>`...`</comment>` :
+//! >> will be removed from the output.
+//! 
+//! > `<+calc tag_name [operator] [operand1] [operand2] [operand3] ... />` :
+//! >> performs a calculation : it applies the operator +, -, *, / or others on one or more operands.
+//! 
+//! Note that these tags are case-insensitive.
+//!
+//! ## Calculations
+//!
+//! The operands of a `<+calc  ... /` tag can be either
+//! - literal numerical values, or else
+//! - tag names that refer to other `<+calc tag_name .../>` tags or `<+actual tag_name>` tags
+//! having that tag name.
+//!
+//! This means that `<+actual>` tags and `<+calc>` tags can't have the same tag name.
+//!
+//! No units may be included in the operands : 10 is allowed, but not 10px.
+//!
+//! Units, however, may follow immediately after `<+calc - .../>` or `<+placeholder .../>` tags. E.g.:
+//!
+//! >`<+actual divHeight>30</+actual>`<br/>
+//! `<+actual goldenCut>1.618034</+actual>`<br/>
+//! `<+calc largeDivHeight * divHeight 2/>`<br/>
+//! `<+calc largeDivWidth / largeDivHeight goldenCut />`<br/>
+//! `...`<br/>
+//! `<+placeholder largeDivWidth/>px`<br/>
+//! `...`<br/>
+//! `<+placeholder largeDivWidth/>px`<br/>
+//! 
+//! A `<+calc tag_name .../>` tag, when evaluated, will be replaced by an `<+/actual tag_name>result</+actual>` tag, except when the tag name (the first word after `<+calc`) is `-`, in which case the result will be directly included in the output file :
+//! 
+//! >`<+actual price>200</+actual>`<br />
+//! `<+actual commission>20</+actual>`<br />
+//! `<+actual taxRate>0.20</+actual>`<br />
+//! `<+calc amount + price commission/> ---> <+actual amount>220</+actual>`<br />
+//! `<+calc tax * amount taxRate/> ---> <+actual>44</+actual>`<br />
+//! `<+calc - + amount tax/> $ ---> 264 $`
+//!
+//! `<+calc>` tags handle the below operators :
+//!
+//! - `+` : addition
+//! - `-` : subtraction
+//! - `*` : multiplication
+//! - `x` : multiplication also
+//! - `/` : division
+//! - `:` : division also
+//! - `÷` : division also
+//! - `%` : remainder
+//! - `min` : get smallest of given operands
+//! - `max` : get greatest of given operands
+//! - `abs` : absolute value
+//! - `^` : power
+//! - `**` : power also
+//! - `pow` : power also
+//! - `exp` : power also
+//! - `sign` : the sign of the first operand.
+//! - `round` : rounding around 0
+//! - `trunc` : removing fractal value, towards 0,
+//! - `floor` : removing fractal value, towards -∞
+//! - `ceiling` : removing fractal value, towards +∞
+//! 
+//! > These operators are case-insensitive.
+//!
+//! > `<+calc>`, `<+actual>` and `<+placeholder>` tags may reside in the same file, in a master
+//! file or in a client file referring to a master file. As `masterpg` operates by joining all
+//! these files' contents to one text before resolving the said tags, you are free to put these
+//! tags wherever they serve your purpose.
+//! 
+//! ## How input files are processed
+//! 
+//! masterpg works by :
+//!
+//! - handling all the input files given on its command line;
+//! 
+//! - executing the below steps for every input file given on the the command line :
+//! 
+//!     - finding the first `<+output>` tag and storing the output file path in a variable;
+//! 
+//!     - then reading an input file in a string and replacing all `<+master />`-tags in this string with the content of the entire file indicated, and even doing this recursively, so even master page files can contain their own `<+master />`-tags;
+//! 
+//!     - then, in the resulting string, replacing
+//!         - all `<+calc>` tags with `<+actual>` tags holding the calculated value.
+//!         - replacing all `<+placeholder>` tags with the content of the corresponding `<+actual>` tags;
+//! 
+//!     - then, removing all the `<+...>` tags (should only be +comment, +output and +actual);
+//! 
+//!     - finally, writing the result to the file path found in the first `<+output>` tag, overwriting existing files.
+//! 
+//! 
+//! As a convention, which is not enforced by `masterpg`, you could use the below file name extensions :
+//! 
+//! - *.mpm for master pages;
+//! - *.mpc for master page consumers (or clients), having an `<+master .../>` tag;
+//! - *.mpx for files that both serve as a master page and consume one themselves.
+//! 
+//! Note that only the name of the final consumer file has to be passed as the first argument to the command
+//! 
+//! `masterpg [consumer file]`
+//!
+//! or, if more than one output file has to be generated :
+//! 
+//! `masterpg [consumer file] [consumer file] [consumer file] ...`
+//! 
+//! ## Example
+//! 
+//! ### Given consts.mpm :
+//!
+//! `<+actual siteName>Test Pages</+actual>`<br />
+//! `<+actual author>Lenny Baxter</+actual>`
+//! 
+//! ### Given general.mpx :
+//! 
+//! `<+master consts.mpm/>`<br />
+//! `<!doctype html>`<br />
+//! `<html>`<br />
+//! `<head>`<br />
+//! `<title><+placeholder pageTitle/></title>`<br />
+//! `</head>`<br />
+//! `<body>`<br />
+//! `<h1><+placeholder pageTitle/></h1>`<br />
+//! `<i>by <+placeholder author/></i>`<br />
+//! `<div id="main"><+placeholder pageContent/></div>`<br />
+//! `</body>`<br />
+//! `</html>`
+//!
+//! ### Given testpg.mpc :
+//! 
+//! `<+master general.mpx/>`<br />
+//! `<+output index.htm/>`<br />
+//! `<+actual pageContent>`<br />
+//! `<p>Welcome to my <+placeholder siteName/> site !</p>`<br />
+//! `<p>I'm <+placeholder author/> and I'm just testing the masterpg module.</p>`<br />
+//! `<p>Bye for now !</p>`<br />
+//! `</+actual>`<br />
+//! `<+actual pageTitle>Welcome</+actual>`
+//! 
+//! ### Then the command
+//!
+//! `masterpg testpg.mpc`
+//! 
+//! ### creates the file index.html :
+//! 
+//! `<!doctype html>`<br />
+//! `<html>`<br />
+//! `<head>`<br />
+//! `<title>Welcome</title>`<br />
+//! `</head>`<br />
+//! `<body>`<br />
+//! `<h1>Welcome</h1>`<br />
+//! `<i>by Lenny Baxter</i>`<br />
+//! `<div id="main">`<br />
+//! `<p>Welcome to my Test Pages site !</p>`<br />
+//! `<p>I'm Lenny Baxter and I'm just testing the masterpg module.</p>`<br />
+//! `<p>Bye for now !</p>`<br />
+//! `</div>`<br />
+//! `</body>`<br />
+//! `</html>`
+//}
 
 use std::cmp::min; //{
 use std::collections::HashMap;
@@ -19,6 +200,39 @@ use nom::{
 };
 use string_io_and_mock::TextIOHandler;
 use tree_by_path::{Node, TraverseAction}; //}
+
+/// See the crate's documentation about functionality.
+pub fn compose<Tioh>(file_path:&OsStr, io_handler: &mut Tioh) -> Result<(), String>
+where Tioh: TextIOHandler {
+    let all_texts:String = read_text_from_handler(io_handler, file_path)?;
+    let mut content_tree = read_nested_content(all_texts.as_str())?;
+    
+    content_tree = resolve_masters_in_tree(io_handler, content_tree)?;
+
+    let recursions = find_recursion_in_content_tree(&mut content_tree);
+    if recursions.len() > 0 {
+        return Err(
+            format!("{} {} {:?}.",
+                "Recursion found in <+placeholder> or <+calc.../>",
+                "tags referring to",
+                recursions
+            ).replace("[", "").replace("]","")
+        );
+    }
+
+    let output_name_string  = find_output_name_in_content_tree(&mut content_tree)?;
+    let output_name = OsStr::new(&output_name_string);
+
+    // Resolve all calculations and placeholders.
+    resolve_references_in_content_tree(&mut content_tree)?;
+
+    let output = content_tree_to_output_string(&mut content_tree);
+
+    match io_handler.write_text(&output_name, output) {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err.to_string())
+    }
+}
 
 #[derive(PartialEq, Debug, Clone)]
 enum Operand {
@@ -92,6 +306,10 @@ enum Operator {
     Abs,
     Exp,
     Sign,
+    Round,
+    Trunc,
+    Floor,
+    Ceiling,
 }
 
 #[derive(PartialEq, Debug, Clone)]
@@ -122,6 +340,10 @@ impl Calculation {
             "abs" => Operator::Abs,
             "exp" | "pow" | "^" | "**" => Operator::Exp,
             "sign" => Operator::Sign,
+            "round" => Operator::Round,
+            "trunc" => Operator::Trunc,
+            "floor" => Operator::Floor,
+            "ceiling" => Operator::Ceiling,
             _ => return Err(CalcError::UnknownOperator(opr_string).to_string()),
         };
 
@@ -152,13 +374,12 @@ impl Calculation {
                 }
 
                 let default_val: f64 = match self.operator {
-                    Operator::Add |
-                    Operator::Subtract |
-                    Operator::Remainder |
-                    Operator::Abs => 0f64,
+                    Operator::Multiply |
+                    Operator::Divide |
+                    Operator::Exp => 1f64,
                     Operator::Min => f64::MAX,
                     Operator::Max => f64::MIN,
-                    _ => 1f64,
+                    _ => 0f64,
                 };
 
                 let opds: Vec<f64> = self.operands.iter()
@@ -171,6 +392,10 @@ impl Calculation {
                 let mut calc_outcome = match self.operator {
                     Operator::Abs => opds[0].abs(),
                     Operator::Sign => opds[0].signum(),
+                    Operator::Round => opds[0].round(),
+                    Operator::Trunc => opds[0].trunc(),
+                    Operator::Floor => opds[0].floor(),
+                    Operator::Ceiling => opds[0].ceil(),
                     _ => opds[0]
                 };
 
@@ -190,9 +415,9 @@ impl Calculation {
                         Operator::Remainder => calc_outcome % opd,
                         Operator::Min => calc_outcome.min(opd),
                         Operator::Max => calc_outcome.max(opd),
-                        Operator::Abs => calc_outcome,
                         Operator::Exp => calc_outcome.powf(opd),
                         Operator::Sign => calc_outcome / opd.signum(),
+                        _ => calc_outcome,
                     };
                 }
 
@@ -866,39 +1091,6 @@ fn content_tree_to_output_string(content_tree: &mut Node<NestedPageContent>) -> 
             }
         }
     ).trim().to_string()
-}
-
-/// io_handler is mutable, for the result is written back to it.
-pub fn compose<Tioh>(file_path:&OsStr, io_handler: &mut Tioh) -> Result<(), String>
-where Tioh: TextIOHandler {
-    let all_texts:String = read_text_from_handler(io_handler, file_path)?;
-    let mut content_tree = read_nested_content(all_texts.as_str())?;
-    
-    content_tree = resolve_masters_in_tree(io_handler, content_tree)?;
-
-    let recursions = find_recursion_in_content_tree(&mut content_tree);
-    if recursions.len() > 0 {
-        return Err(
-            format!("{} {} {:?}.",
-                "Recursion found in <+placeholder> or <+calc.../>",
-                "tags referring to",
-                recursions
-            ).replace("[", "").replace("]","")
-        );
-    }
-
-    let output_name_string  = find_output_name_in_content_tree(&mut content_tree)?;
-    let output_name = OsStr::new(&output_name_string);
-
-    // Resolve all calculations and placeholders.
-    resolve_references_in_content_tree(&mut content_tree)?;
-
-    let output = content_tree_to_output_string(&mut content_tree);
-
-    match io_handler.write_text(&output_name, output) {
-        Ok(_) => Ok(()),
-        Err(err) => Err(err.to_string())
-    }
 }
 
 #[cfg(test)]
@@ -1963,6 +2155,150 @@ This is a test page.
 
         let outcome = c.get_value();
         assert_eq!(CalculationValue::Resolved(1_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_round_positive_small() {
+        let words = vec!["test", "round", "56.41"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(56_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_round_positive_large() {
+        let words = vec!["test", "round", "56.5"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(57_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_round_negative_small() {
+        let words = vec!["test", "round", "-6.41"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(-6_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_round_negative_large() {
+        let words = vec!["test", "round", "-6.5"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(-7_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_trunc_positive_small() {
+        let words = vec!["test", "trunc", "56.41"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(56_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_trunc_positive_large() {
+        let words = vec!["test", "trunc", "56.5"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(56_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_trunc_negative_small() {
+        let words = vec!["test", "trunc", "-6.41"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(-6_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_trunc_negative_large() {
+        let words = vec!["test", "trunc", "-6.5"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(-6_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_floor_positive_small() {
+        let words = vec!["test", "floor", "56.41"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(56_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_floor_positive_large() {
+        let words = vec!["test", "floor", "56.5"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(56_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_floor_negative_small() {
+        let words = vec!["test", "floor", "-6.41"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(-7_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_floor_negative_large() {
+        let words = vec!["test", "floor", "-6.5"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(-7_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_ceiling_positive_small() {
+        let words = vec!["test", "ceiling", "56.41"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(57_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_ceiling_positive_large() {
+        let words = vec!["test", "ceiling", "56.5"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(57_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_ceiling_negative_small() {
+        let words = vec!["test", "ceiling", "-6.41"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(-6_f64), outcome);
+    }
+
+    #[test]
+    fn calc_resolve_ceiling_negative_large() {
+        let words = vec!["test", "ceiling", "-6.5"];
+        let mut c = Calculation::new(words.iter().map(|w| w.to_string()).collect()).unwrap();
+
+        let outcome = c.get_value();
+        assert_eq!(CalculationValue::Resolved(-6_f64), outcome);
     }
 
     #[test]
